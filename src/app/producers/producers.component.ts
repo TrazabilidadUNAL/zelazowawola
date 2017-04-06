c798db8caa21e833b41e4072aaab5ec91d0f0283
Andres Forero <afforeroc@unal.edu.co>
2017-04-06 14:37:11 -0500

    Producer request done
    The basic requests by id on the producer table are almost working
    
    https://trello.com/c/qFJIuyqY/66-conectar-aplicacion-con-back-end

--- src/app/producers/producers.component.ts
+++ src/app/producers/producers.component.ts
@@ -1,18 +1,43 @@
+// Imports
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Producer } from './producer';
+import { Observable } from 'rxjs/Rx';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-producers',
   templateUrl: './producers.component.html',
   styleUrls: ['./producers.component.css']
 })
-export class ProducersComponent implements OnInit {
-
-  message: string;
-  constructor(private http: Http) {
-    http.get('http://localhost:3000/v1/producers/1')
-      .subscribe(res => this.message = res.json());
+export class ProducersComponent implements OnInit{
+  data: Object;
+  show: boolean;
+  constructor(public http: Http) {
   }
+  private commentsUrl = 'http://localhost:3000/v1/producers';
+  getProducer1(form: any): void {
+    var id = form["sky"];
+    this.show = true;
+    this.http.request(`${this.commentsUrl}/${id}`)
+      .subscribe((res: Response) => {
+        this.data = res.json();
+        this.show = false;
+      });
+  }
+
+  /**
+  getProducer2(): void {
+    this.show = true;
+    this.http.request(`${this.producersUrl}`)
+      .subscribe((res: Response) => {
+        this.data = res.json();
+        this.show = false;
+      });
+  }**/
 
   ngOnInit() {
   }
