dafd166b26107a647e6aaddd383ddebdac274248
Luis Eduardo Martin <lemartinp@unal.edu.co>
2017-04-07 01:42:21 -0500

    Register account is working
    Now we can successfully create accounts for warehouses and producers
    
    https://trello.com/c/FCBNtB0u/73-account

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import {Producer} from './../producers/producer';
+import {Warehouse} from './../warehouse/warehouse';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -30,6 +31,24 @@ export class AccountComponent implements OnInit {
     //   .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
     //   .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     this.http.post('http://localhost:3000/v1/producers', JSON.stringify(producer1), {headers: headers})
+      .map(res => res.json())
+      .subscribe(
+        data => console.log(data)
+      );
+  }
+
+  postWarehouse(form: any): void {
+    const name = form['wName'];
+    const username = form['wUserame'];
+    const password = form['wPassword'];
+    const warehouse1 = new Warehouse(name, username, password);
+    console.log(JSON.stringify(warehouse1));
+    const headers = new Headers({'Content-Type': 'application/json; charset=utf-8'});
+    const options = new RequestOptions({headers: headers});
+    // this.http.post(`${this.producersUrl}`, JSON.stringify(producer1), options)
+    //   .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+    //   .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    this.http.post('http://localhost:3000/v1/warehouses', JSON.stringify(warehouse1), {headers: headers})
       .map(res => res.json())
       .subscribe(
         data => console.log(data)
