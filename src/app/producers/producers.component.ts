99b7f813c260312ab8351fb1c276d95d3dc5d214
Mesi Rendon <amrendonsa@unal.edu.co>
2017-04-06 22:15:26 -0500

    Fix on post request using angular's http
    We have changed the way angular's http has to do a post request.
    
    Pair programming with https://github.com/afforeroc
    
    https://trello.com/c/qFJIuyqY/

--- src/app/producers/producers.component.ts
+++ src/app/producers/producers.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import { Producer } from './producer';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import {Producer} from './producer';
 
 // Import RxJs required methods
 import 'rxjs/add/operator/map';
@@ -12,25 +12,31 @@ import 'rxjs/add/operator/catch';
   templateUrl: './producers.component.html',
   styleUrls: ['./producers.component.css']
 })
-export class ProducersComponent implements OnInit{
+export class ProducersComponent implements OnInit {
   data: Object;
+
   constructor(public http: Http) {
   }
-  private producersUrl = 'http://localhost:3000/v1/producers';
 
   getProducer(form: any): void {
     const id = form['GetProducer'];
-    this.http.request(`${this.producersUrl}/${id}`)
-      .subscribe((res: Response) => this.data = res.json(), );
+    this.http.request(`http://localhost:3000/v1/producers/${id}`)
+      .subscribe((res: Response) => this.data = res.json());
   }
 
   postProducer(): void {
-    const producer1 = new Producer('andres', 'forero', 'afforeroc', 'hshdjn43985');
-    const headers = new Headers({ 'Content-Type': 'application/json; charset=utf-8' });
-    const options = new RequestOptions({ headers: headers });
-    this.http.post('${this.producersUrl}', JSON.stringify(producer1), options)
-      .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
-      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    const producer1 = new Producer('andres3', 'forero3', 'afforeroc', 'hshdjn43985');
+    console.log(JSON.stringify(producer1));
+    const headers = new Headers({'Content-Type': 'application/json; charset=utf-8'});
+    const options = new RequestOptions({headers: headers});
+    // this.http.post(`${this.producersUrl}`, JSON.stringify(producer1), options)
+    //   .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+    //   .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    this.http.post('http://localhost:3000/v1/producers', JSON.stringify(producer1), {headers: headers})
+      .map(res => res.json())
+      .subscribe(
+        data => console.log(data)
+      );
   }
 
   ngOnInit() {
