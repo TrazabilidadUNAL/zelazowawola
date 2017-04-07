af8a225cdbae1c37266c01aacb59736b277676f6
Luis Eduardo Martin <lemartinp@unal.edu.co>
2017-04-07 02:28:42 -0500

    POST for warehouses is working
    Now the only thing left to do is make it look good for the judges
    
    https://trello.com/c/874ORqWw/68-warehouse
    form['GetWarehouse']

--- src/app/warehouse/warehouse.component.ts
+++ src/app/warehouse/warehouse.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import {Warehouse} from './warehouse';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 @Component({
   selector: 'app-warehouse',
   templateUrl: './warehouse.component.html',
@@ -7,7 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarehouseComponent implements OnInit {
 
-  constructor() { }
+  data: Object;
+
+  constructor(public http: Http) { }
+
+  getWarehouse(form: any): void {
+    const id = form['GetWarehouse'];
+    this.http.request(`http://localhost:3000/v1/warehouses/${id}`)
+      .subscribe((res: Response) => this.data = res.json());
+  }
 
   ngOnInit() {
   }
