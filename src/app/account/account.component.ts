ef16d45849e1569741bcb188e2bcae0fe9ba3745
Andres Forero <afforeroc@unal.edu.co>
2017-04-23 01:31:39 -0500

    PUT and DELETE methods for Producer
    - Put and Delete methods for Producer were added.
    - Fixed some code's elements of Account Component and Producer Component.
    
    https://trello.com/c/qFJIuyqY/66-conectar-aplicacion-con-back-end
    
    We worked together: afforeroc amrendonsa

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import {Observable} from 'rxjs/Rx';
 import {Producer} from '../producer/producer';
 import {Warehouse} from './../warehouse/warehouse';
+//import {Observable} from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -12,44 +12,37 @@ import 'rxjs/add/operator/catch';
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
+
 export class AccountComponent implements OnInit {
-
-  data: Object;
-
-  constructor(public http: Http) { }
+  constructor(public http: Http) {}
 
   postProducer(form: any): void {
-    const name = form['pName'];
-    const lastname = form['pLastName'];
-    const username = form['pUserame'];
+    const first_name = form['pFirstName'];
+    const last_name = form['pLastName'];
+    const username = form['pUsername'];
     const password = form['pPassword'];
-    const producer1 = new Producer(name, lastname, username, password);
-    console.log(JSON.stringify(producer1));
+    const pModel = new Producer(first_name, last_name, username, password);
     const headers = new Headers({'Content-Type': 'application/json; charset=utf-8'});
     const options = new RequestOptions({headers: headers});
+
+    console.log(JSON.stringify(pModel));
     this.http
-      .post('http://localhost:3000/v1/producers', JSON.stringify(producer1), {headers: headers})
-      .map(res => res.json())
-      .subscribe(
-        data => console.log(data)
-      );
+      .post('http://localhost:3000/v1/producers', JSON.stringify(pModel), options)
+      .map(res => res.json()).subscribe(data => console.log(data));
   }
 
   postWarehouse(form: any): void {
     const name = form['wName'];
-    const username = form['wUserame'];
+    const username = form['wUsername'];
     const password = form['wPassword'];
-    const warehouse1 = new Warehouse(name, username, password);
-    console.log(JSON.stringify(warehouse1));
+    const wModel = new Warehouse(name, username, password);
     const headers = new Headers({'Content-Type': 'application/json; charset=utf-8'});
     const options = new RequestOptions({headers: headers});
 
+    console.log(JSON.stringify(wModel));
     this.http
-      .post('http://localhost:3000/v1/warehouses', JSON.stringify(warehouse1), {headers: headers})
-      .map(res => res.json())
-      .subscribe(
-        data => console.log(data)
-      );
+      .post('http://localhost:3000/v1/warehouses', JSON.stringify(wModel), options)
+      .map(res => res.json()).subscribe(data => console.log(data));
   }
 
   ngOnInit() {
--- src/app/producer/producer.component.ts
+++ src/app/producer/producer.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import {Observable} from 'rxjs/Rx';
 import {Producer} from './producer';
+//import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-producer',
@@ -9,31 +9,40 @@ import {Producer} from './producer';
   styleUrls: ['./producer.component.css']
 })
 export class ProducerComponent implements OnInit {
-  data: Object;
-
-  constructor(public http: Http) {
-  }
+  dProducer: Object;
+  urlProducer: string = 'http://localhost:3000/v1/producers';
+  constructor(public http: Http) {}
 
   getProducer(form: any): void {
     const id = form['GetProducer'];
-    this.http.get(`http://localhost:3000/v1/producers/${id}`)
-        .subscribe((res: Response) => this.data = res.json());
-    console.log(this.data);
+
+    this.http.get(`${this.urlProducer}/${id}`)
+        .subscribe((res: Response) => this.dProducer = res.json());
+    console.log(this.dProducer);
   }
 
-  postProducer(): void {
-    const producer1 = new Producer('andres3', 'forero3', 'afforeroc', 'hshdjn43985');
-    console.log(JSON.stringify(producer1));
+  putProducer(form: any): void {
+    const id = form['pID'];
+    const first_name = form['pFirstName'];
+    const last_name = form['pLastName'];
+    const username = form['pUsername'];
+    const password = form['pPassword'];
+    const pModel = new Producer(first_name, last_name, username, password);
     const headers = new Headers({'Content-Type': 'application/json; charset=utf-8'});
     const options = new RequestOptions({headers: headers});
-    // this.http.post(`${this.producersUrl}`, JSON.stringify(producer1), options)
-    //   .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
-    //   .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    this.http.post('http://localhost:3000/v1/producers', JSON.stringify(producer1), {headers: headers})
-        .map(res => res.json())
-        .subscribe(
-            data => console.log(data)
-        );
+
+    console.log(JSON.stringify(pModel));
+    this.http
+      .put(`${this.urlProducer}/${id}`, JSON.stringify(pModel), options)
+      .map(res => res.json()).subscribe( data => console.log(data));
+  }
+
+  deleteProducer(form: any): void {
+    const id = form['DeleteProducer'];
+
+    this.http.delete(`${this.urlProducer}/${id}`)
+      .subscribe((res: Response) => this.dProducer = res.json());
+    console.log(this.dProducer);
   }
 
   ngOnInit() {
