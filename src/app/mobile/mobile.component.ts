684c2f8a4da97b73154a88e45af32a5a68dc4962
Luis Eduardo Martin <lemartinp@unal.edu.co>
2017-05-04 15:05:42 -0500

    Added crop logs on mobile view
    the only thing left is to give post-like style to the croplogs and make
    the markers appear on the map
    
    https://trello.com/c/09FCnRXS/83-view-for-mobile-device

--- src/app/mobile/mobile.component.ts
+++ src/app/mobile/mobile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {Route} from './mobile';
+import {Log} from './croplogs';
 
 @Component({
   selector: 'app-mobile',
@@ -10,31 +11,56 @@ import {Route} from './mobile';
 export class MobileComponent implements OnInit {
 
   lat: number = 51.678418;
-  lng: number = 7.809007;
+  lon: number = 7.809007;
 
-  id: string;
-  origin_id: string;
+  
 
   dRoute: Object;
+  cropLogs: Object;
 
-  getRoute(form: any): void {
-    const id = form['GetRoute'];
+  getRoute(): void {
+    const id = Math.random() * 100;
 
-    this.http.get(`http://localhost:3000/v1/routes/1`)
+    this.http.get(`http://localhost:3000/v1/routes/${id}`)
         .subscribe((res: Response) => this.dRoute = res.json());
     console.log(this.dRoute);
   }
-/*
+
+  //prints latitude and longitude list
   selectedRoute: Route;
+  
   onSelectRo(route: Route): void {
     this.selectedRoute = route;
-    this.id = route.id;
-    this.origin_id = route.origin_id;
+    this.lat = Number(route.lat);
+    this.lon = Number(route.lon);
   }
-*/
+
+  getCropLogs(): void {
+    const id = Math.random() * 100;
+
+    this.http.get(`http://localhost:3000/v1/crops/${id}`)
+        .subscribe((res: Response) => this.cropLogs = res.json());
+    console.log(this.cropLogs);
+  }
+
+  // prints list of logs
+  selectedLog: Log;
+  logId: number;
+  logDescription: string;
+  logCreatedAt: string;
+
+  onSelectLog(log: Log): void {
+    this.selectedLog = log;
+    this.logId = Number(log.id);
+    this.logDescription = log.description;
+    this.logCreatedAt = log.created_at;
+
+  }
   constructor(public http: Http) { }
 
   ngOnInit() {
+    this.getRoute();
+    this.getCropLogs();
   }
 
 
